35203998133d3edc0c2d2782f4cd7e476b664ac9
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-08-08 15:35:43 -0400

    icons

--- client/src/app/(dev)/style-guide/icons.tsx
+++ client/src/app/(dev)/style-guide/icons.tsx
@@ -1,10 +1,94 @@
 import React from "react";
-import UploadIcon from "~/icons/upload.icon";
+import DocumentAddIcon from "~/icons/document-add.icon";
+import UploadIcon from "~/icons/cloud-upload.icon";
+import ArrowExpandIcon from "~/icons/arrow-expand.icon";
+import ArrowRightIcon from "~/icons/arrow-right.icon";
+import BackIcon from "~/icons/back.icon";
+import CheckCircleIcon from "~/icons/check-circle.icon";
+import CheckIcon from "~/icons/check.icon";
+import ChevronDownIcon from "~/icons/chevron-down.icon";
+import ChevronUpIcon from "~/icons/chevron-up.icon";
+import ClockIcon from "~/icons/clock.icon";
+import CodeIcon from "~/icons/code.icon";
+import DatabaseIcon from "~/icons/database.icon";
+import ExclamationIcon from "~/icons/exclamation.icon";
+import ExportIcon from "~/icons/export.icon";
+import FlagIcon from "~/icons/flag.icon";
+import InformationCircle from "~/icons/information-circle";
+import KeyIcon from "~/icons/key.icon";
+import KeyboardCommandIcon from "~/icons/keyboard-command.icon";
+import MinusIcon from "~/icons/minus.icon";
+import NextIcon from "~/icons/next.icon";
+import NoteIcon from "~/icons/note.icon";
+import PencilIcon from "~/icons/pencil.icon";
+import PlusIcon from "~/icons/plus.icon";
+import RefereshIcon from "~/icons/referesh.icon";
+import SearchIcon from "~/icons/search.icon";
+import ShuffleIcon from "~/icons/shuffle.icon";
+import SliderIcon from "~/icons/slider.icon";
+import SortIcon from "~/icons/sort.icon";
+import SpaceBarIcon from "~/icons/space-bar.icon";
+import SummaryIcon from "~/icons/summary.icon";
+import TrashIcon from "~/icons/trash.icon";
+import UnorderedListIcon from "~/icons/unordered-list.icon";
+import ValuesIcon from "~/icons/values.icon";
+import ViewGridIcon from "~/icons/view-grid.icon";
+import XCircleIcon from "~/icons/x-circle.icon";
+import XIcon from "~/icons/x.icon";
+import H from "~/components/h";
 
 export default function Icons() {
   return (
-    <div className="flex-1 gap-4">
-      <UploadIcon />
+    <div className="flex flex-col gap-4">
+      <H variant="h4">Icons</H>
+      <div className="flex w-fit flex-col gap-4 rounded-xl border p-4">
+        <div className="flex flex-row items-center justify-center gap-4">
+          <UploadIcon />
+          <DocumentAddIcon />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <ArrowExpandIcon />
+          <ArrowRightIcon />
+          <BackIcon />
+          <CheckCircleIcon />
+          <CheckIcon />
+          <ChevronDownIcon />
+          <ChevronUpIcon />
+          <ClockIcon />
+          <CodeIcon />
+          <DatabaseIcon />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <ExclamationIcon />
+          <ExportIcon />
+          <FlagIcon />
+          <InformationCircle />
+          <KeyIcon />
+          <KeyboardCommandIcon />
+          <MinusIcon />
+          <NextIcon />
+          <NoteIcon />
+          <PencilIcon />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <PlusIcon />
+          <RefereshIcon />
+          <SearchIcon />
+          <ShuffleIcon />
+          <SliderIcon />
+          <SortIcon />
+          <SpaceBarIcon />
+          <SummaryIcon />
+          <TrashIcon />
+          <UnorderedListIcon />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <ValuesIcon />
+          <ViewGridIcon />
+          <XCircleIcon />
+          <XIcon />
+        </div>
+      </div>
     </div>
   );
 }
