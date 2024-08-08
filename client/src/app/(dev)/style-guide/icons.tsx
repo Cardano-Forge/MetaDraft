bacf8df6c3c9f1d463fb5e42f35e37614944ad27
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-08-08 14:50:13 -0400

    h tag

---
+++ client/src/app/(dev)/style-guide/icons.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UploadIcon from "~/icons/upload.icon";
+
+export default function Icons() {
+  return (
+    <div className="flex-1 gap-4">
+      <UploadIcon />
+    </div>
+  );
+}
