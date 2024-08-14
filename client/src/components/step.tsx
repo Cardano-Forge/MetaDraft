f22b3b486dacb15652adf3fc56d83cf6ec27c4ce
pabloCC <chaussepablo@gmail.com>
2024-08-14 14:37:16 -0400

    search param status

--- client/src/components/step.tsx
+++ client/src/components/step.tsx
@@ -1,34 +1,41 @@
+"use client";
 import React from "react";
 import { cn } from "~/lib/utils";
 import { Typography } from "./typography";
 import CheckIcon from "~/icons/check.icon";
+import { useSearchParams } from "next/navigation";
 
 export type StepStatus = "active" | "done" | "next";
 
 export type StepProps = {
   id: number;
-  status: StepStatus;
+  status?: StepStatus;
   className?: string;
   children?: React.ReactNode;
 };
 
 const variant: Record<StepStatus, string> = {
   active: "bg-secondary",
-  done: "bg-background",
+  done: "bg-card",
   next: "bg-transparent border border-border/20 border-dashed",
 };
 
 export default function Step({ id, status, className, children }: StepProps) {
+  const searchParams = useSearchParams();
+  const activeStep = Number(searchParams.get("s") ?? 1);
+  status =
+    status ?? (activeStep < id ? "next" : activeStep > id ? "done" : "active");
   const isDone = status === "done";
   const isActive = status === "active";
   const isNext = status === "next";
   return (
-    <div
+    <a
       className={cn(
-        "flex w-full max-w-[224px] flex-col justify-between gap-2 rounded-3xl p-4",
+        "flex w-full max-w-[224px] flex-col justify-between gap-6 rounded-3xl p-4",
         variant[status],
         className,
       )}
+      href={`?s=${id}`}
     >
       <div
         className={cn(
@@ -45,6 +52,6 @@ export default function Step({ id, status, className, children }: StepProps) {
       >
         {children}
       </Typography>
-    </div>
+    </a>
   );
 }
