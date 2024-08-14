78b4596269d36bcf1b08bc758b65161765a8b2e5
pabloCC <chaussepablo@gmail.com>
2024-08-14 14:32:45 -0400

    fix build

--- client/src/app/(dev)/style-guide/steps-guide.tsx
+++ client/src/app/(dev)/style-guide/steps-guide.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import Step from "~/components/steps";
+import Step from "~/components/step";
 import { Typography } from "~/components/typography";
 
 export default function StepsGuide() {
   return (
     <div className="flex flex-col gap-4">
       <Typography as="h2">Steps</Typography>
-      <div className="flex w-full flex-row gap-8 rounded-xl border p-4 bg-[#444444]">
-        <Step id={1} status="done" text="Keys and values validation" />
-        <Step id={2} status="active" text="NFTs data validation" />
-        <Step id={3} status="next" text="Compare thumbnail and HR images" />
-        <Step id={4} status="next" text="Final validation" />
+      <div className="flex w-full flex-row gap-8 rounded-xl border bg-[#444444] p-4">
+        <Step id={1} status="done">
+          Keys and values validation
+        </Step>
+        <Step id={2} status="active">
+          NFTs data validation
+        </Step>
+        <Step id={3} status="next">
+          Compare thumbnail and HR images
+        </Step>
+        <Step id={4} status="next">
+          Final validation
+        </Step>
       </div>
     </div>
   );
