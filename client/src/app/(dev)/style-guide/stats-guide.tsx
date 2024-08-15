179ce28136bd1f73c64bfb6cf0923e3ee7dca900
pabloCC <chaussepablo@gmail.com>
2024-08-15 09:32:10 -0400

    refactor stat comp

--- client/src/app/(dev)/style-guide/stats-guide.tsx
+++ client/src/app/(dev)/style-guide/stats-guide.tsx
@@ -1,45 +1,27 @@
 import React from "react";
-import Stats from "~/components/stats";
+import Stat from "~/components/stat";
 import { Typography } from "~/components/typography";
-import CheckIcon from "~/icons/check.icon";
-import ClockIcon from "~/icons/clock.icon";
-import DatabaseIcon from "~/icons/database.icon";
-import ExclamationIcon from "~/icons/exclamation.icon";
-import FlagIcon from "~/icons/flag.icon";
 
 export default function StatsGuide() {
   return (
     <div className="flex flex-col gap-4">
       <Typography as="h2">Stats</Typography>
       <div className="flex w-full flex-row gap-8 rounded-xl border p-4">
-        <Stats
-          icon={<DatabaseIcon className="h-4 w-4" />}
-          stat={10_000}
-          text="NFTs in this collection"
-        />
-        <Stats
-          icon={<ClockIcon className="h-4 w-4" />}
-          stat={9_950}
-          text="NFTs unchecked"
-        />
-        <Stats
-          icon={<ExclamationIcon className="h-[16px] w-[18px]" />}
-          stat={2}
-          text="Errors detected"
-          iconType="error"
-        />
-        <Stats
-          icon={<FlagIcon className="h-4 w-4" />}
-          stat={3}
-          text="Errors flagged"
-          iconType="warning"
-        />
-        <Stats
-          icon={<CheckIcon className="h-4 w-4" />}
-          stat={50}
-          text="Marked as valid"
-          iconType="success"
-        />
+        <Stat icon="database" stat={10_000}>
+          NFTs in this collection
+        </Stat>
+        <Stat icon="clock" stat={9_950}>
+          NFTs unchecked
+        </Stat>
+        <Stat icon="exclamation" stat={2} variant="error">
+          Errors detected
+        </Stat>
+        <Stat icon="flag" stat={3} variant="warning">
+          Errors flagged
+        </Stat>
+        <Stat icon="check" stat={50} variant="success">
+          Marked as valid
+        </Stat>
       </div>
     </div>
   );
