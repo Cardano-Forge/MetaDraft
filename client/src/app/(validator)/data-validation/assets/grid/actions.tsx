1f0e877c965c70b6d39faffe78ed068900fda5a5
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-08-23 17:01:08 -0400

    Grid view

---
+++ client/src/app/(validator)/data-validation/assets/grid/actions.tsx
@@ -0,0 +1,32 @@
+import { Button } from "~/components/ui/button";
+import CheckIcon from "~/icons/check.icon";
+import FlagIcon from "~/icons/flag.icon";
+import { type Status } from "~/lib/types";
+
+type ActionsType = {
+  state: Status;
+};
+
+export default function Actions({ state }: ActionsType) {
+  const isSuccess = state === "success";
+  const isWarning = state === "warning";
+
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Button
+        variant={isWarning ? "warning" : "warningOutilne"}
+        size={"icon"}
+        onClick={() => alert("Should set this asset status to WARNING")}
+      >
+        <FlagIcon className="h-4 w-4" />
+      </Button>
+      <Button
+        variant={isSuccess ? "success" : "successOutline"}
+        size={"icon"}
+        onClick={() => alert("Should set this asset status to SUCCESS")}
+      >
+        <CheckIcon className="h-4 w-4" />
+      </Button>
+    </div>
+  );
+}
