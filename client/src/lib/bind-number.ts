87447b108cf57b6d6a1e870c5768da89ad74f9a8
pabloCC <chaussepablo@gmail.com>
2024-09-03 14:36:27 -0400

    format

--- client/src/lib/bind-number.ts
+++ client/src/lib/bind-number.ts
@@ -1,9 +1,9 @@
 export function bind(min: number, max: number, value: number) {
-    if (value < min) {
-      return min;
-    }
-    if (value > max) {
-      return max;
-    }
-    return value;
+  if (value < min) {
+    return min;
   }
+  if (value > max) {
+    return max;
+  }
+  return value;
+}
