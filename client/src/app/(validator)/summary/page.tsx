e4bffef7e6d1c1fc3aec2154420b823a1db404f3
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-09-05 15:05:53 -0400

    feat: base component

--- client/src/app/(validator)/summary/page.tsx
+++ client/src/app/(validator)/summary/page.tsx
@@ -1,12 +1,21 @@
 import { Typography } from "~/components/typography";
+import Summary from "./summary";
 
 export default function SummaryPage() {
   return (
-    <>
-      <Typography as="h2">Summary</Typography>
-      <Typography as="p" className="text-sm text-white/50">
-        step desc
-      </Typography>
-    </>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-col gap-4">
+          <Typography as="h2">Project summary</Typography>
+          <Typography as="p" className="text-sm text-white/50">
+            Small description lorem ipsum dolor
+          </Typography>
+        </div>
+        <div className="flex flex-row items-center gap-4">
+          {/* Add buttons here */}
+        </div>
+      </div>
+      <Summary />
+    </div>
   );
 }
---
+++ client/src/app/(validator)/summary/summary/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Summary() {
+  return <div className="flex flex-col rounded-2xl bg-card p-4">summary</div>;
+}
