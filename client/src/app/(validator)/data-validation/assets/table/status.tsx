f6cdf2ff5ac60b942abf1c7f35813a060d210baf
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-09-09 10:49:47 -0400

    fix: un-use import

--- client/src/app/(validator)/data-validation/assets/table/status.tsx
+++ client/src/app/(validator)/data-validation/assets/table/status.tsx
@@ -10,10 +10,8 @@ import {
 import { Button, type ButtonVariants } from "~/components/ui/button";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
