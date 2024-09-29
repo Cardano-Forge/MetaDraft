52596e0314929fe0b44e0d91f247e92287e1a044
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-09-29 14:41:02 -0400

    feat: delete from string array

--- client/src/app/(asset)/metadata/[id]/content/json-editor.tsx
+++ client/src/app/(asset)/metadata/[id]/content/json-editor.tsx
@@ -100,9 +100,19 @@ export default function JSONEditor({
     }
   };
 
-  const handleOnUpdate: UpdateFunction = ({ newData }) => {
-    console.log("ON EDIT");
-    // todo check newData === template key:valueType
+  const handleOnUpdate: UpdateFunction = ({
+    newData,
+    name,
+    path,
+    newValue,
+  }) => {
+    if (
+      typeof name === "number" &&
+      typeof newValue === "string" &&
+      newValue.length === 0
+    ) {
+      deleteByPath(newData as Omit<MetadataCollection, "id" | "status">, path);
+    }
 
     // Zod Validation on update
     const zodResults = MetadataCollectionSchemav2.safeParse(newData);
@@ -129,13 +139,13 @@ export default function JSONEditor({
         To edit a key, double-click it. Press Enter to save, or Esc to cancel.
       </Typography>
       <Typography className="italic text-white/50">
-        To edit a value, double-click it.
+        To delete an element from string array. Set the value to an empty string.
       </Typography>
       <JsonEditor
         data={meta}
         showErrorMessages
         enableClipboard={false}
-        defaultValue={""}
+        defaultValue={"null"}
         rootFontSize={18}
         minWidth={"100%"}
         collapse={3}
@@ -150,4 +160,28 @@ export default function JSONEditor({
       />
     </div>
   );
+}
+
+/**
+ * Function to delete a value by path in a type-safe way
+ */
+function deleteByPath<T extends object>(
+  obj: T,
+  path: (string | number)[],
+): void {
+  const lastKey = path.pop(); // Remove the last key (index 1 in this case)
+
+  // Access the parent of the value we want to delete
+  const parent = path.reduce<unknown>((acc, key) => {
+    if (acc && (typeof acc === "object" || Array.isArray(acc))) {
+      return acc[key as keyof typeof acc];
+    }
+    return undefined;
+  }, obj);
+
+  if (parent && typeof lastKey === "number" && Array.isArray(parent)) {
+    parent.splice(lastKey, 1); // Remove the element at the array index
+  } else if (parent && typeof lastKey === "string") {
+    delete parent[lastKey as keyof typeof parent]; // Delete the key if it's an object
+  }
 }
