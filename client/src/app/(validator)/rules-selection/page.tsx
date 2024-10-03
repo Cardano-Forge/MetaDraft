a53abc5ae737a2e926d69b1aa0897da12e5a43b8
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-10-03 10:59:52 -0400

    feat: UI change

--- client/src/app/(validator)/rules-selection/page.tsx
+++ client/src/app/(validator)/rules-selection/page.tsx
@@ -1,22 +1,10 @@
 import { Typography } from "~/components/typography";
-import { mapping } from "@ada-anvil/metadraft-validator";
-import { ruleSet } from "~/lib/constant";
-import { RULES_DESCRIPTION, type RulesId } from "~/lib/rules";
-import { Switch } from "~/components/ui/switch";
-import { Label } from "~/components/ui/label";
-import { camelCaseToTitleCase } from "~/lib/camel-case-to-title-case";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
+
+import Content from "./content";
+import Footer from "./footer";
+import Header from "./header";
 
 export default function RulesPage() {
-  const keys = Object.keys(mapping) as RulesId[];
-
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-row items-center justify-between">
@@ -31,29 +19,10 @@ export default function RulesPage() {
         </div>
       </div>
 
-      <div className="rounded-xl border border-white bg-secondary">
-        <Table className="rounded-2xl">
-          <TableHeader className="h-14 rounded-2xl bg-background hover:bg-background">
-            <TableRow className="rounded-2xl bg-background hover:bg-background">
-              <TableHead className="min-w-[150px] rounded-tl-2xl">Rules Name</TableHead>
-              <TableHead>Definition</TableHead>
-              <TableHead className="rounded-tr-2xl">Active</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {keys.map((key) => (
-              <TableRow key={key} className="bg-background/50">
-                <TableCell className="font-medium">
-                  <Label>{camelCaseToTitleCase(key)}</Label>
-                </TableCell>
-                <TableCell>{RULES_DESCRIPTION[key]}</TableCell>
-                <TableCell>
-                  <Switch checked={ruleSet.includes(key)} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+      <div className="flex flex-col rounded-2xl bg-card">
+        <Header />
+        <Content />
+        <Footer />
       </div>
     </div>
   );
