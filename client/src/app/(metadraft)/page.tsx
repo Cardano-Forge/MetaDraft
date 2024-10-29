b463539eb10da80abd4808b85193ab9825d2681a
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-10-28 20:40:47 -0400

    init: react-joyride

--- client/src/app/(metadraft)/page.tsx
+++ client/src/app/(metadraft)/page.tsx
@@ -1,11 +1,25 @@
+import Joyride from "react-joyride";
+
 import CreateProjectButton from "~/components/create-project-button";
 import { MetadataTemplate } from "~/components/metadata-template";
 import { Typography } from "~/components/typography";
 import { UploadProjectButton } from "~/components/upload-project-button";
 
+const steps = [
+  {
+    target: ".my-first-step",
+    content: "This is my awesome feature!",
+  },
+  {
+    target: ".my-other-step",
+    content: "This another awesome feature!",
+  },
+];
+
 export default function HomePage() {
   return (
     <main className="container flex flex-col gap-8">
+      <Joyride steps={steps} />
       <div className="mt-4 flex flex-col items-center justify-center lg:flex-row lg:justify-between">
         <div className="flex flex-row items-center justify-center lg:flex-col lg:items-start">
           <Typography as="h1" className="font-inter font-bold tracking-wide">
