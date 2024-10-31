f973640e143a6b8a099c7fd82a25a98a4790a012
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-10-31 14:26:12 -0400

    fix build + Simplier Widget

--- client/src/components/userback.tsx
+++ client/src/components/userback.tsx
@@ -1,32 +1,13 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-
-import Userback, { type UserbackWidget } from "@userback/widget";
-
+import { useEffect } from "react";
+import Userback from "@userback/widget";
 import { env } from "~/env";
-import { useActiveProject } from "~/providers/active-project.provider";
 
 const UserbackAnchor = () => {
-  const userback = useRef<UserbackWidget>();
-  const activeProject = useActiveProject();
-  const project = activeProject?.toJSON();
-
   useEffect(() => {
-    if (!project) return;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    Userback(env.NEXT_PUBLIC_USERBACK_ID).then((ub) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      userback.current = ub;
-      const data = {
-        user_data: {
-          project,
-        },
-      };
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-      ub.identify(project.id, data);
-    });
-  }, [project]);
+    void Userback(env.NEXT_PUBLIC_USERBACK_ID); // Now TypeScript recognizes the type
+  }, []);
 
   return null;
 };
