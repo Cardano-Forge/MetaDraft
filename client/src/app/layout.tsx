5c67e066b193d478ed02f1db3997ff982b2dd471
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-11-15 13:47:27 -0500

    add GA

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
-
+import { env } from "~/env";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,6 +22,23 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable}`}>
       <body className="flex min-h-screen flex-col overflow-x-hidden bg-background antialiased">
+        <Script
+          async
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${env.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${env.GA_TRACKING_ID}');
+            `,
+          }}
+        />
         {children}
       </body>
     </html>
