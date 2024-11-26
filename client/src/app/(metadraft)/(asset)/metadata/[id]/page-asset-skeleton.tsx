d504df9fe362d525e889bc26d21e558169d2f5b0
Frederic Gour-Bonin <fred.gbonin@gmail.com>
2024-11-26 11:26:07 -0500

    fix: build

--- client/src/app/(metadraft)/(asset)/metadata/[id]/page-asset-skeleton.tsx
+++ client/src/app/(metadraft)/(asset)/metadata/[id]/page-asset-skeleton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type Dispatch, type SetStateAction } from "react";
 
 import Header from "./header";
 import { Skeleton } from "~/components/ui/skeleton";
@@ -14,7 +14,16 @@ export default function PageAssetSkeleton({
   return (
     <div className="pt-5">
       <div className="container">
-        <Header metadata={metadata} isValidating={isValidating} />
+        <Header
+          metadata={metadata}
+          isValidating={isValidating}
+          hasUnsavedChanges={false}
+          setHasUnsavedChanges={
+            (() => {
+              console.log("cannot be empty");
+            }) as Dispatch<SetStateAction<boolean>>
+          }
+        />
       </div>
       <main className="border-t border-white/15 py-8">
         <div className="container">
